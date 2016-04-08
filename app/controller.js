f7dc80cede698552e8d462d67742d7730cd834b9
Orifhon <hitadorg@gmail.com>
2016-04-08 18:12:29 -0400

    ngstatic-include initial commit

---
+++ app/controller.js
@@ -0,0 +1,4 @@
+angular.module("staticTemplate").controller("Controller", ["$scope", function($scope){
+    $scope.text = "This is a test message";
+    $scope.text2 = "This is a test message for in-main body";
+}]);
